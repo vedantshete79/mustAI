54b78cd2b6b178818257f4d64f043bd570e38340
mustAI <vedantshete79@gmail.com>
2026-07-11 22:10:30 +0530

    Time: 0 ms (100%), Space: 9 MB (38.12%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char ch : s) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } 
+            else {
+                if (st.empty())
+                    return false;
+
+                if ((ch == ')' && st.top() == '(') ||
+                    (ch == '}' && st.top() == '{') ||
+                    (ch == ']' && st.top() == '[')) {
+                    st.pop();
+                } 
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
