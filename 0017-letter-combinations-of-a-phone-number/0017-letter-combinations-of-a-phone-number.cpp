5799a9df05d6d631e0949def8e1c208449f76e50
mustAI <vedantshete79@gmail.com>
2026-07-11 22:05:04 +0530

    Time: 0 ms (100%), Space: 9.5 MB (21.31%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<string> ans;
+
+    vector<string> keypad = {
+        "",     // 0
+        "",     // 1
+        "abc",  // 2
+        "def",  // 3
+        "ghi",  // 4
+        "jkl",  // 5
+        "mno",  // 6
+        "pqrs", // 7
+        "tuv",  // 8
+        "wxyz"  // 9
+    };
+
+    void backtrack(string &digits, int index, string current) {
+        if (index == digits.size()) {
+            ans.push_back(current);
+            return;
+        }
+
+        string letters = keypad[digits[index] - '0'];
+
+        for (char ch : letters) {
+            backtrack(digits, index + 1, current + ch);
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty())
+            return {};
+
+        backtrack(digits, 0, "");
+
+        return ans;
+    }
+};
